8a00231fcc3f04776ddf3ab9d9bc33e4967c6542
Murisi Tarusenga <murisit@gmail.com>
2018-09-14 12:07:33 +0200

    Put the memory allocations of execute_macro into the right regions.

--- src/preparer.c
+++ src/preparer.c
@@ -383,39 +383,39 @@ struct compilation {
 	void *macro;
 };
 
-void *execute_macro(list (*expander)(list), union expression *np, list *ext_binds, list st_binds, list dyn_refs, list *comps, region comps_reg, myjmp_buf *handler) {
+void *execute_macro(list (*expander)(list), union expression *np, list *ext_binds, list st_binds, list dyn_refs, list *compilations, region rt_reg, myjmp_buf *handler) {
 	list np_expanded = expander(np->non_primitive.argument);
 	struct compilation *pc;
-	{foreach(pc, *comps) {
+	{foreach(pc, *compilations) {
 		if(np == pc->np_expression && sexpr_list_equals(np_expanded, pc->np_expanded)) {
 			return pc->macro;
 		}
 	}}
 	
-	region reg = create_region(0);
-	union expression *func = make_function(comps_reg), *ref;
-	{foreach(ref, reverse(dyn_refs, comps_reg)) {
-		union expression *param = copy_expression(ref, reg);
-		prepend(param, &func->function.parameters, reg);
+	region ct_reg = create_region(0);
+	union expression *func = make_function(ct_reg), *ref;
+	{foreach(ref, reverse(dyn_refs, ct_reg)) {
+		union expression *param = copy_expression(ref, ct_reg);
+		prepend(param, &func->function.parameters, ct_reg);
 		param->reference.parent = func;
 	}}
-	put(func, function.expression, build_syntax_tree(np_expanded, comps_reg, handler));
+	put(func, function.expression, build_syntax_tree(np_expanded, ct_reg, handler));
 	{foreach(ref, func->function.parameters) {
-		put(func, function.expression, insert_indirections(func->function.expression, ref, reg));
+		put(func, function.expression, insert_indirections(func->function.expression, ref, ct_reg));
 	}}
 	{foreach(ref, np->non_primitive.indirections) {
-		put(func, function.expression, insert_indirections(func->function.expression, ref, reg));
+		put(func, function.expression, insert_indirections(func->function.expression, ref, ct_reg));
 	}}
-	Object *obj = load_expressions(lst(func, nil(reg), reg), ext_binds, st_binds, comps, comps_reg, handler);
+	Object *obj = load_expressions(lst(func, nil(ct_reg), ct_reg), ext_binds, st_binds, compilations, rt_reg, handler);
 	mutate_symbols(obj, *ext_binds);
 	mutate_symbols(obj, st_binds);
 	
-	pc = region_malloc(comps_reg, sizeof(struct compilation));
+	pc = region_malloc(rt_reg, sizeof(struct compilation));
 	pc->np_expression = np;
 	pc->np_expanded = np_expanded;
 	//There is only one immutable symbol: our annonymous function
-	pc->macro = ((Symbol *) immutable_symbols(obj, reg)->fst)->address;
-	prepend(pc, comps, comps_reg);
-	destroy_region(reg);
+	pc->macro = ((Symbol *) immutable_symbols(obj, ct_reg)->fst)->address;
+	prepend(pc, compilations, rt_reg);
+	destroy_region(ct_reg);
 	return pc->macro;
 }
