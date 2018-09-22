c8f9fb632c48d9bf8bb136453de162d308eaef08
Murisi Tarusenga <murisit@gmail.com>
2018-09-22 12:41:43 +0200

    Translated read_object into L2.

--- assets/x86_64.s
+++ assets/x86_64.s
@@ -1,11 +1,13 @@
 .global getb
 .global get1b
 .global get2b
+.global get4b
 .global get8b
 .global get
 .global setb
 .global set1b
 .global set2b
+.global set4b
 .global set8b
 .global set
 .global add
@@ -43,6 +45,11 @@ xorq %rax, %rax
 movw 0(%rdi), %ax
 ret
 
+get4b:
+xorq %rax, %rax
+movl 0(%rdi), %eax
+ret
+
 set:
 set8b:
 movq %rsi, 0(%rdi)
@@ -57,6 +64,11 @@ ret
 set2b:
 movq %rsi, %rax
 movw %ax, 0(%rdi)
+ret
+
+set4b:
+movq %rsi, %rax
+movl %eax, 0(%rdi)
 ret
 
 add:
