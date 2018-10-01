9aac9dc2bf46511b1c58a54381797987ed9b20c3
Murisi Tarusenga <murisit@gmail.com>
2018-10-01 09:33:57 +0200

    Use the character structure instead of the sexpr union for character array.

--- src/lexer.c
+++ src/lexer.c
@@ -17,23 +17,24 @@ bool char_equals(struct character *a, struct character *b) {
 	return a->character == b->character;
 }
 
-#define char_init(ch) {{ .character = { .list_flag = NULL, .character = ch }}}
+#define char_init(ch) {{ .list_flag = NULL, .character = ch }}
 
-union sexpr chars[][1] = {
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, char_init('!'),
-	char_init('"'), char_init('#'), char_init('$'), char_init('%'), char_init('&'), char_init('\''), 0, 0, char_init('*'),
+struct character chars[][1] = {
+	{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, {0},
+	{0}, {0}, {0}, {0}, {0}, {0}, {0}, {0}, char_init('!'),
+	char_init('"'), char_init('#'), char_init('$'), char_init('%'), char_init('&'), char_init('\''), {0}, {0}, char_init('*'),
 	char_init('+'), char_init(','), char_init('-'), char_init('.'), char_init('/'), char_init('0'), char_init('1'),
 	char_init('2'), char_init('3'), char_init('4'), char_init('5'), char_init('6'), char_init('7'), char_init('8'),
 	char_init('9'), char_init(':'), char_init(';'), char_init('<'), char_init('='), char_init('>'), char_init('?'),
 	char_init('@'), char_init('A'), char_init('B'), char_init('C'), char_init('D'), char_init('E'), char_init('F'),
 	char_init('G'), char_init('H'), char_init('I'), char_init('J'), char_init('K'), char_init('L'), char_init('M'),
 	char_init('N'), char_init('O'), char_init('P'), char_init('Q'), char_init('R'), char_init('S'), char_init('T'),
-	char_init('U'), char_init('V'), char_init('W'), char_init('X'), char_init('Y'), char_init('Z'), 0, char_init('\\'),
-	0, char_init('^'), char_init('_'), char_init('`'), char_init('a'), char_init('b'), char_init('c'), char_init('d'),
+	char_init('U'), char_init('V'), char_init('W'), char_init('X'), char_init('Y'), char_init('Z'), {0}, char_init('\\'),
+	{0}, char_init('^'), char_init('_'), char_init('`'), char_init('a'), char_init('b'), char_init('c'), char_init('d'),
 	char_init('e'), char_init('f'), char_init('g'), char_init('h'), char_init('i'), char_init('j'), char_init('k'),
 	char_init('l'), char_init('m'), char_init('n'), char_init('o'), char_init('p'), char_init('q'), char_init('r'),
 	char_init('s'), char_init('t'), char_init('u'), char_init('v'), char_init('w'), char_init('x'), char_init('y'),
-	char_init('z'), 0, char_init('|'), 0, char_init('~'), 0
+	char_init('z'), {0}, char_init('|'), {0}, char_init('~'), {0}
 };
 
 #define _char(d) (chars[d])
