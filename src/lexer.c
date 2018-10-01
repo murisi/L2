e0549a015a61728078a70120d4b80cfe3aa63966
Murisi Tarusenga <murisit@gmail.com>
2018-10-01 09:19:53 +0200

    Renamed some functions in lexer.c.

--- src/lexer.c
+++ src/lexer.c
@@ -13,6 +13,31 @@ bool is_lst(union sexpr *s) {
 	return s->list_flag ? true : false;
 }
 
+bool char_equals(struct character *a, struct character *b) {
+	return a->character == b->character;
+}
+
+#define char_init(ch) {{ .character = { .list_flag = NULL, .character = ch }}}
+
+union sexpr chars[][1] = {
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, char_init('!'),
+	char_init('"'), char_init('#'), char_init('$'), char_init('%'), char_init('&'), char_init('\''), 0, 0, char_init('*'),
+	char_init('+'), char_init(','), char_init('-'), char_init('.'), char_init('/'), char_init('0'), char_init('1'),
+	char_init('2'), char_init('3'), char_init('4'), char_init('5'), char_init('6'), char_init('7'), char_init('8'),
+	char_init('9'), char_init(':'), char_init(';'), char_init('<'), char_init('='), char_init('>'), char_init('?'),
+	char_init('@'), char_init('A'), char_init('B'), char_init('C'), char_init('D'), char_init('E'), char_init('F'),
+	char_init('G'), char_init('H'), char_init('I'), char_init('J'), char_init('K'), char_init('L'), char_init('M'),
+	char_init('N'), char_init('O'), char_init('P'), char_init('Q'), char_init('R'), char_init('S'), char_init('T'),
+	char_init('U'), char_init('V'), char_init('W'), char_init('X'), char_init('Y'), char_init('Z'), 0, char_init('\\'),
+	0, char_init('^'), char_init('_'), char_init('`'), char_init('a'), char_init('b'), char_init('c'), char_init('d'),
+	char_init('e'), char_init('f'), char_init('g'), char_init('h'), char_init('i'), char_init('j'), char_init('k'),
+	char_init('l'), char_init('m'), char_init('n'), char_init('o'), char_init('p'), char_init('q'), char_init('r'),
+	char_init('s'), char_init('t'), char_init('u'), char_init('v'), char_init('w'), char_init('x'), char_init('y'),
+	char_init('z'), 0, char_init('|'), 0, char_init('~'), 0
+};
+
+#define _char(d) (chars[d])
+
 list copy_sexpr_list(list l, region r) {
 	list c = nil;
 	union sexpr *s;
@@ -20,45 +45,16 @@ list copy_sexpr_list(list l, region r) {
 		if(is_lst(s)) {
 			append(copy_sexpr_list((list) s, r), &c, r);
 		} else {
-			union sexpr *t = region_alloc(r, sizeof(union sexpr));
-			t->character = s->character;
-			append(t, &c, r);
+			append(_char(s->character.character), &c, r);
 		}
 	}
 	return c;
 }
 
-bool char_equals(struct character *a, struct character *b) {
-	return a->character == b->character ? true : false;
-}
-
-#define char_sexpr(ch) {{ .character = { .list_flag = NULL, .character = ch }}}
-
-union sexpr character_sexprs[][1] = {
-	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, char_sexpr('!'),
-	char_sexpr('"'), char_sexpr('#'), char_sexpr('$'), char_sexpr('%'), char_sexpr('&'), char_sexpr('\''), 0, 0, char_sexpr('*'),
-	char_sexpr('+'), char_sexpr(','), char_sexpr('-'), char_sexpr('.'), char_sexpr('/'), char_sexpr('0'), char_sexpr('1'),
-	char_sexpr('2'), char_sexpr('3'), char_sexpr('4'), char_sexpr('5'), char_sexpr('6'), char_sexpr('7'), char_sexpr('8'),
-	char_sexpr('9'), char_sexpr(':'), char_sexpr(';'), char_sexpr('<'), char_sexpr('='), char_sexpr('>'), char_sexpr('?'),
-	char_sexpr('@'), char_sexpr('A'), char_sexpr('B'), char_sexpr('C'), char_sexpr('D'), char_sexpr('E'), char_sexpr('F'),
-	char_sexpr('G'), char_sexpr('H'), char_sexpr('I'), char_sexpr('J'), char_sexpr('K'), char_sexpr('L'), char_sexpr('M'),
-	char_sexpr('N'), char_sexpr('O'), char_sexpr('P'), char_sexpr('Q'), char_sexpr('R'), char_sexpr('S'), char_sexpr('T'),
-	char_sexpr('U'), char_sexpr('V'), char_sexpr('W'), char_sexpr('X'), char_sexpr('Y'), char_sexpr('Z'), 0, char_sexpr('\\'),
-	0, char_sexpr('^'), char_sexpr('_'), char_sexpr('`'), char_sexpr('a'), char_sexpr('b'), char_sexpr('c'), char_sexpr('d'),
-	char_sexpr('e'), char_sexpr('f'), char_sexpr('g'), char_sexpr('h'), char_sexpr('i'), char_sexpr('j'), char_sexpr('k'),
-	char_sexpr('l'), char_sexpr('m'), char_sexpr('n'), char_sexpr('o'), char_sexpr('p'), char_sexpr('q'), char_sexpr('r'),
-	char_sexpr('s'), char_sexpr('t'), char_sexpr('u'), char_sexpr('v'), char_sexpr('w'), char_sexpr('x'), char_sexpr('y'),
-	char_sexpr('z'), 0, char_sexpr('|'), 0, char_sexpr('~'), 0
-};
-
-union sexpr *build_character_sexpr(char d) {
-	return character_sexprs[d];
-}
-
 list build_symbol_sexpr(char *str, region r) {
 	list sexprs = nil;
 	for(; *str; str++) {
-		append(build_character_sexpr(*str), &sexprs, r);
+		append(_char(*str), &sexprs, r);
 	}
 	return sexprs;
 }
@@ -126,7 +122,7 @@ list build_expr_list(char *l2src, int l2src_sz, int *pos, region r, jumpbuf *han
 	} else {
 		list l = nil;
 		do {
-			append(build_character_sexpr(c), &l, r);
+			append(_char(c), &l, r);
 			if(*pos == l2src_sz) return l;
 			c = l2src[(*pos)++];
 		} while(!isspace(c) && c != '(' && c != ')' && c != '{' && c != '}' && c != '[' && c != ']');
